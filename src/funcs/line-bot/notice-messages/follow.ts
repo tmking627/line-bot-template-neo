a1d47d261593ebc8f54ce04c4ca12da7c0858f23
tmking627 <tmy6254789@gmail.com>
2023-10-31 23:58:58 +0900

    メッセージ追加

--- src/funcs/line-bot/notice-messages/follow.ts
+++ src/funcs/line-bot/notice-messages/follow.ts
@@ -4,3 +4,13 @@ export const msgFollow: TextMessage = {
   type: 'text',
   text: 'フォローありがとう！'
 }
+
+export const msgFollow2: TextMessage = {
+  type: 'text',
+  text: 'フォロー雛形その２'
+}
+
+export const msgFollow3: TextMessage = {
+  type: 'text',
+  text: 'フォロー雛形その３'
+}
